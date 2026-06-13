4d01ca2111066f0d0de1b62ad135decf727aa917
Karthik <gantalamadhanakumari@gmail.com>
2026-06-13 13:48:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -83,3 +83,38 @@ function addMessage(sender,text,className){
     chatBox.appendChild(div);
     chatBox.scrollTop = chatBox.scrollHeight;
 }
+const response = await fetch(
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
+{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        contents: [{
+            parts: [{
+                text: message
+            }]
+        }]
+    })
+});
+
+const data = await response.json();
+
+console.log(data);
+
+if (data.error) {
+    addMessage("Bot", data.error.message, "bot");
+    return;
+}
+
+const reply =
+data.candidates &&
+data.candidates[0] &&
+data.candidates[0].content &&
+data.candidates[0].content.parts &&
+data.candidates[0].content.parts[0]
+? data.candidates[0].content.parts[0].text
+: "No response from Gemini";
+
+addMessage("Bot", reply, "bot");
